bfa5a01d52f25d395845543144a3460445c57ef9
saharsch8 <saharsch8@gmail.com>
2023-08-25 19:54:19 +0300

    ChatClient: change main to handle controllers

--- src/chat_client.cpp
+++ src/chat_client.cpp
@@ -121,9 +121,9 @@ int main(int argc, char* argv[])
 {
   try
   {
-    if (argc != 2)
+    if (argc != 3)
     {
-      std::cerr << "Usage: chat_client <host> <port>\n";
+      std::cerr << "Usage: chat_client <port> <type>\n";
       return 1;
     }
 
@@ -134,20 +134,40 @@ int main(int argc, char* argv[])
     chat_client c(io_context, endpoints);
 
     std::thread t([&io_context](){ io_context.run(); });
+    std::string port = argv[1];
+    std::string type = argv[2];
 
     // char line[chat_message::max_body_length + 1];
-    std::string line;
-    while (std::getline(std::cin, line))
-    {
-      sb::Event e("room_1", "NOISE");
-      e.event_warper(line);
+    if (port == "7070") {
+      std::string line;
 
+      while (std::getline(std::cin, line))
+      {
+        sb::Event e("room_1", type);
+        e.event_warper(line);
+
+        chat_message msg;
+        msg.body_length(line.size());
+        std::memcpy(msg.body(), line.c_str(), msg.body_length());
+        msg.encode_header();
+        c.write(msg);
+      }
+    }
+
+    if (port == "8080") {
+      std::string line = "x";
+
+      sb::Event e("subscribe", type);
+      e.event_warper(line);
 
       chat_message msg;
       msg.body_length(line.size());
       std::memcpy(msg.body(), line.c_str(), msg.body_length());
       msg.encode_header();
+      std::this_thread::sleep_for(std::chrono::seconds(1));
       c.write(msg);
+
+      while(true);
     }
 
     c.close();
