c2af300d1b2a0baa53a0a9ebf8a6fe50be4c7ec2
saharsch8 <saharsch8@gmail.com>
2023-08-25 19:53:27 +0300

    SubscribeManager: change class members and function to handle controller subscription and delivering messeges

--- src/subscribe_manager.cpp
+++ src/subscribe_manager.cpp
@@ -1,27 +1,33 @@
 #include "subscribe_manager.hpp"
 
 #include <iostream>
+#include <algorithm>
 
 namespace sb {
 
-void SubscribeManager::join(SubscriberPtr a_subscriber)
+void SubscribeManager::join(SubscriberPtr a_subscriber, Protocol const& a_event)
 {
-    m_subscriber_clan[a_subscriber->event_type()] = a_subscriber;
+    if (std::string type(a_event.event_type()); type != "") {
+        m_subscriber_clan[type].push_back(a_subscriber);
+        std::cout << "subscribed to" << type << "\n";
+    }
 }
 
-void SubscribeManager::leave(SubscriberPtr a_subscriber) //needed?
+void SubscribeManager::leave(SubscriberPtr a_subscriber) 
 {
-    m_subscriber_clan.erase(a_subscriber->event_type());
+    for (auto type_subscribers : m_subscriber_clan) {
+        type_subscribers.second.erase(std::find(type_subscribers.second.begin(), type_subscribers.second.end(), a_subscriber));
+    }
 }
 
-void SubscribeManager::deliver(Protocol const& a_event) // TODO: change to "get_new_event" and seperate thread to handle
-                                                        // events (???).
+void SubscribeManager::deliver(Protocol const& a_event) 
 {
     m_recent_events.push_back(a_event);
-    std::cout << a_event.event_type() << "\n";
-    std::cout << a_event.event_location() << "\n";
-    for (auto subscriber : m_subscriber_clan)
-        subscriber.second->deliver(a_event);
+    std::string type = a_event.event_type();
+
+    for (auto subscriber : m_subscriber_clan[type]) {
+        subscriber->deliver(a_event);
+    }
 }
   
 }// namespace sb
