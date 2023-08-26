8f8f67adab6833f7c4a0536c22bfe8ea92999609
saharsch8 <saharsch8@gmail.com>
2023-08-26 22:33:13 +0300

    UDP: update

--- src/udp.server.cpp
+++ src/udp.server.cpp
@@ -1,29 +1,53 @@
+#include "client.hpp"
+#include "event.hpp"
+
 #include <cstdlib>
 #include <iostream>
 #include <boost/asio.hpp>
 
 using boost::asio::ip::udp;
 
-enum { max_length = 1024 };
+enum { max_length = 1024 }; 
 
 class UdpListner {
-public:
+public: 
     UdpListner(boost::asio::io_context& io_context, unsigned short port)
-    :m_socket(io_context, udp::endpoint(udp::v4(), port))
+    : m_socket(io_context, udp::endpoint(udp::v4(), port))
     {
     }
 
-    void receive_data(std::stringstream& a_buffer)
+    void receive_data()
     {
         char data[max_length];
         udp::endpoint sender_endpoint;
+		boost::asio::io_context send_io_context;
+
+		tcp::resolver resolver(send_io_context);
+		auto endpoints = resolver.resolve("", "7070");
+		sb::Client c(send_io_context, endpoints);
+
+		std::thread t([&send_io_context](){ send_io_context.run(); });
+		std::string type = "1";
+		
         for (;;){
             size_t length =  m_socket.receive_from(boost::asio::buffer(data, max_length), sender_endpoint);   
             std::string line(data);
             line = line.substr(0, length);
             std::cout << line << "\n";
-            a_buffer << line;
+
+			std::string event = line;
+            sb::Event e("room_1", type);
+            e.event_warper(event);
+
+            chat_message msg;
+            msg.body_length(event.size());
+            std::memcpy(msg.body(), event.c_str(), msg.body_length());
+            msg.encode_header();
+            c.write(msg);
         }
+
+		c.close();
+    	t.join();
     }
 
 private:
@@ -32,21 +56,15 @@ private:
 };
 
 
+
 int main()
 {
   try
   {
-    // if (argc != 2)
-    // {
-    //      ::cerr << "Usage: blocking_udp_echo_server <port>\n";
-    //   return 1;
-    // }
-
     boost::asio::io_context io_context;
 
     UdpListner ul(io_context, 9090);
-    std::stringstream buffer;
-    ul.receive_data(buffer);
+    ul.receive_data();
   }
 catch (std::exception& e)
   {
