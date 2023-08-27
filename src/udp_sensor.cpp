6a6f23d8c37f5300f2bf5f13d285c4fd45f34361
din <din1.plombo@gmail.com>
2023-08-27 10:41:04 +0300

    clean code

--- src/udp_sensor.cpp
+++ src/udp_sensor.cpp
@@ -1,33 +1,32 @@
-#include <cstdlib>
-#include <cstring>
+#include <string> 
 #include <iostream>
-#include <thread> 
-#include <chrono>
+#include <thread> //sleep_for
+#include <chrono> 
+
 #include <boost/asio.hpp>
 
 using boost::asio::ip::udp;
 
 int main()
 {
-  try
-  {
-    boost::asio::io_context io_context;
-    udp::socket s(io_context, udp::endpoint(udp::v4(), 0));
-    udp::resolver resolver(io_context);
-    udp::resolver::results_type endpoints = resolver.resolve(udp::v4(), "", "9090");
-    
-    while(true){
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        char request[] = "40";
-        size_t request_length = std::strlen(request);
-        s.send_to(boost::asio::buffer(request, request_length), *endpoints.begin());
-    }
+	try
+	{
+		boost::asio::io_context io_context;
+		udp::socket s(io_context, udp::endpoint(udp::v4(), 0));
+		udp::resolver resolver(io_context);
+		udp::resolver::results_type endpoints = resolver.resolve(udp::v4(), "", "9090");
 
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << "Exception: " << e.what() << "\n";
-  }
+		while(true){
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+			std::string data = "40";
+			s.send_to(boost::asio::buffer(data.c_str(), data.size()), *endpoints.begin());
+		}
 
-  return 0;
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << "\n";
+	}
+
+	return 0;
 }
