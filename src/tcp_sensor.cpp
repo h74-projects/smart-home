6d6ab45ca4ef7750df9474b9e051c4c2f3fc0659
din <din1.plombo@gmail.com>
2023-08-29 15:16:18 +0300

    clean code

--- src/tcp_sensor.cpp
+++ src/tcp_sensor.cpp
@@ -1,37 +1,3 @@
-// #include <string> 
-// #include <iostream>
-// #include <thread> //sleep_for
-// #include <chrono> 
-
-// #include <boost/asio.hpp>
-
-// using boost::asio::ip::udp;
-
-// int main()
-// {
-// 	try
-// 	{
-// 		boost::asio::io_context io_context;
-// 		udp::socket s(io_context, udp::endpoint(udp::v4(), 0));
-// 		udp::resolver resolver(io_context);
-// 		udp::resolver::results_type endpoints = resolver.resolve(udp::v4(), "", "7070");
-
-// 		while(true){
-// 			std::this_thread::sleep_for(std::chrono::seconds(1));
-// 			std::string data = "40";
-// 			s.send_to(boost::asio::buffer(data.c_str(), data.size()), *endpoints.begin());
-// 		}
-
-// 	}
-// 	catch (std::exception& e)
-// 	{
-// 		std::cerr << "Exception: " << e.what() << "\n";
-// 	}
-
-// 	return 0;
-// }
-
-
 #include <string> 
 #include <iostream>
 #include <thread> //sleep_for
