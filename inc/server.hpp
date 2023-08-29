c732c7ea5113c5ed4a0e37bfea40f3c90ede9991
din <din1.plombo@gmail.com>
2023-08-29 14:15:37 +0300

    server clean code

--- inc/server.hpp
+++ inc/server.hpp
@@ -13,19 +13,15 @@ namespace sb {
 class Server {
 public:
     explicit Server(boost::asio::io_context& a_io_context, Agent& a_agent, bool a_device_type, SubscribeManager& a_sm);
-    ~Server() noexcept = default;
     
-    Server(Server const& a_other) = delete;
-    Server& operator=(Server const& a_other) = delete;
+    ~Server() noexcept = default;
 
 private:
     void accept();
     
 private:
     tcp::acceptor m_acceptor;
-    // tcp::acceptor m_sensor_acceptor;
     Agent& m_agent;
-    // Agent& m_agent_controler;
     bool m_device_type;
     SubscribeManager& m_subscribe_manager;
 };
