9f1d97e46acf7b36f87cb38115019babca611568
din <din1.plombo@gmail.com>
2023-08-29 14:12:35 +0300

    main clean code

--- tests/demo/main.cpp
+++ tests/demo/main.cpp
@@ -1,18 +1,12 @@
 #include "server.hpp"
-
 #include "agent_tempature.hpp"
 #include "agent_ac.hpp"
 #include "agent.hpp"
 #include "subscribe_manager.hpp"
 
-#include <iostream>
 #include <exception>
-#include <memory>
-#include <thread>
-#include <unordered_map>
-#include <vector>
-#include <string>
-#include <iostream>
+#include <memory> //unique_ptr
+#include <iostream> //std::cerr
 #include <fstream> //ifstream
 #include <nlohmann/json.hpp> //nlohmann::json
 #include <stdexcept> //runtime error
@@ -22,30 +16,13 @@
 using boost::asio::ip::tcp;
 using namespace sb;
 
-typedef std::unordered_map<std::string, std::vector<std::string>> ControlersAgents; 
-
-void set_controler_agents(ControlersAgents& a_controler_agents)
-{
-    std::ifstream sensors_id_file("../../assets/agents.dat");
-    if(!sensors_id_file) {
-        std::runtime_error("file opcode_map.dat could not be opened!");
-    }
-    nlohmann::json json_sendors_id;
-    sensors_id_file >> json_sendors_id;
-    a_controler_agents = json_sendors_id.get<ControlersAgents>();
-}
-
-
 int main()
 {
     try{
-        // ControlersAgents controler_agents;
-        // set_controler_agents(controler_agents);
-
         SubscribeManager sm;
 
         std::unique_ptr<Agent> agent_ptr = std::make_unique<AgentTempature>("../../assets/sensor_id.dat");
-        std::unique_ptr<Agent> agent_controler_ptr = std::make_unique<AgentAc>("../../assets/sensor_id.dat");//all the controlers
+        std::unique_ptr<Agent> agent_controler_ptr = std::make_unique<AgentAc>("../../assets/sensor_id.dat");//TODO controlers for agent?
 
         boost::asio::io_context io_context;
         Server s_controler(io_context, *(dynamic_cast<AgentAc*>(agent_controler_ptr.get())), false, sm);
