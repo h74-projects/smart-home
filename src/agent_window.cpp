35a13a4953cdc449bd6714768964b9a0172d3d02
din <din1.plombo@gmail.com>
2023-09-05 00:43:03 +0300

    bug in display log

--- src/agent_window.cpp
+++ src/agent_window.cpp
@@ -26,11 +26,18 @@ unsigned short AgentWindow::sender_port() const
 
 bool AgentWindow::check_event(Protocol const& a_event, Protocol& a_command)
 {
-	char command[] = "close window";
+	char command_close[] = "close window";
+	char command_open[] = "open window";
 	
-	if(a_event.event_data() == "40"){
-		a_command.body_length(std::strlen(command));
-		std::memcpy(a_command.body(), command, a_command.body_length());
+	if(a_event.event_data() == "30"){
+		a_command.body_length(std::strlen(command_close));
+		std::memcpy(a_command.body(), command_close, a_command.body_length());
+		a_command.encode_header();
+		return true;
+
+	} else if(a_event.event_data() == "25") {
+		a_command.body_length(std::strlen(command_open));
+		std::memcpy(a_command.body(), command_open, a_command.body_length());
 		a_command.encode_header();
 		return true;
 	}
